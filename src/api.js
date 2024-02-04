0bc339bcdef28d054e0f376c6143d20a140efb79
Mehdi <cpourl.arjen@gmail.com>
2024-02-04 17:04:57 +0100

    étape 3

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import CryptoJS from 'crypto-js';
 
 /**
  * Récupère les données de l'endpoint en utilisant les identifiants
@@ -7,7 +8,14 @@ import fetch from 'node-fetch';
  * @return {Promise<json>}
  */
 export const getData = async (url) => {
-    // A Compléter
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
 }
 
 /**
@@ -19,5 +27,34 @@ export const getData = async (url) => {
  * @return {Promise<ArrayBuffer>} en hexadecimal
  */
 export const getHash = async (publicKey, privateKey, timestamp) => {
-    // A compléter
+    try {
+        const hash = CryptoJS.MD5(`${timestamp}${privateKey}${publicKey}`).toString();
+        return hash;
+    } catch (error) {
+        console.error('Error generating hash:', error);
+        throw error;
+    }
+}
+
+const publicKey = "4127f72f71e72c08b584350b149981f6";
+const privateKey = "51420b3b2ced406ece06dd286c9b5d4bda5c9b3b";
+const ts = Date.now();
+
+const hash = await getHash(publicKey, privateKey, ts);
+console.log(ts);
+console.log(hash);
+
+const url = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}&name=Hulk`;
+const marvelData = await getData(url);
+
+
+
+console.log(marvelData);
+
+const thorData = marvelData.data.results.find(character => character.name === 'Hulk');
+
+if (thorData) {
+    console.log('Données de Thor:', thorData);
+} else {
+    console.log('Thor n\'a pas été trouvé dans les résultats.');
 }
